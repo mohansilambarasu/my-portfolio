147b7bd4f1a016fa6c19adbfb69f4bfd260416e2
mohansilambarasu <mohansilambu24@gmail.com>
2025-03-31 23:27:45 -0400

    Added loader feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
 import { HashRouter, Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
@@ -10,8 +10,18 @@ import { Contact } from "./components/Contact";
 import { Hero } from "./components/Hero";
 import { Home } from "./pages/Home";
 import StarCanvas from "./components/StarCanvas";
+import Loader from "./components/Loader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) return <Loader />;
+
   return (
     <>
       <BrowserRouter>
---
+++ src/components/Loader.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { FaRocket, FaStar, FaMeteor } from "react-icons/fa";
+
+const Loader = () => {
+  const [currentIcon, setCurrentIcon] = useState(0);
+  const icons = [<FaRocket />, <FaStar />, <FaMeteor />];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIcon((prev) => (prev + 1) % icons.length);
+    }, 500);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-black text-white">
+      <div className="text-5xl animate-spin">{icons[currentIcon]}</div>
+      <p className="ml-4 text-lg">Loading...</p>
+    </div>
+  );
+};
+
+export default Loader;
